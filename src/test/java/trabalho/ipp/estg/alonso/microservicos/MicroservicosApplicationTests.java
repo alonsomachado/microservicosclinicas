3822ddbd5bca8171a1a71ddc7a118a8ffbe441b1
alonsomachado <alonsomachado2@hotmail.com>
2019-12-17 15:04:08 +0000

    Spring Boot JAVA 8 MAVEN e JPA
    Spring Boot IO com JAVA 8 e coloquei JPA também.... MAVEN

---
+++ src/test/java/trabalho/ipp/estg/alonso/microservicos/MicroservicosApplicationTests.java
@@ -0,0 +1,13 @@
+package trabalho.ipp.estg.alonso.microservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

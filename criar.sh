2ea67f2197e94537917e7414de7166751e892bf5
alonsomachado <alonsomachado2@hotmail.com>
2020-01-13 18:43:18 +0000

    realizar porto
    realizar porto

--- criar.sh
+++ criar.sh
@@ -3,5 +3,6 @@ kubectl apply -f ./confminikube/redisvolume.yaml
 kubectl apply -f ./confminikube/redis.yaml
 docker build -t micro-agendamento:v1 ./agendamento
 docker build -t micro-pagamento:v1 ./pagamento
+docker build -t micro-realizar:v1 ./agendamento
 minikube addons enable ingress
 kubectl apply -f ./confminikube

5872dc95134e94e5e6e80ebe0145c86699617107
alonsomachado <alonsomachado2@hotmail.com>
2020-01-14 14:05:51 +0000

    Front Express Handlebars Bootstrap
    Front Express Handlebars Bootstrap

--- criar.sh
+++ criar.sh
@@ -1,8 +1,9 @@
 eval $(minikube docker-env)
-kubectl apply -f ./confminikube/redisvolume.yaml
-kubectl apply -f ./confminikube/redis.yaml
+kubectl apply -f ./confminikube/redispubsub.yaml
+kubectl apply -f ./confminikube/rediscaduser.yaml
 docker build -t micro-agendamento:v1 ./agendamento
 docker build -t micro-pagamento:v1 ./pagamento
 docker build -t micro-realizar:v1 ./realizar
+docker build -t micro-front:v1 . ./front
 minikube addons enable ingress
 kubectl apply -f ./confminikube
